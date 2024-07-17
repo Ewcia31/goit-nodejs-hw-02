b2693f90a2755ee1a652f8ef2941e61cae365cda
Ewcia31 <ewa.krzystolik@gmail.com>
2024-07-17 21:28:06 +0200

    Adding user authentication/authorization logic

--- controller/contacts.js
+++ controller/contacts.js
@@ -10,14 +10,13 @@ const contactSchema = Joi.object({
 
 const get = async (req, res, next) => {
   try {
-    const results = await service.getAllContacts();
+    const results = await service.getAllContacts(req.user._id);
     res.json({
       status: "success",
       code: 200,
       data: { contacts: results.map((contact) => contact.toJSON()) },
     });
   } catch (error) {
-    console.error("Error fetching contacts: ", error);
     next(error);
   }
 };
@@ -25,7 +24,7 @@ const get = async (req, res, next) => {
 const getById = async (req, res, next) => {
   const { id } = req.params;
   try {
-    const result = await service.getContactById(id);
+    const result = await service.getContactById(req.user._id, id);
     if (result) {
       res.json({
         status: "success",
@@ -41,13 +40,11 @@ const getById = async (req, res, next) => {
       });
     }
   } catch (error) {
-    console.error("Error finding contact: ", error);
     next(error);
   }
 };
 
 const create = async (req, res, next) => {
-  const { name, email, phone, favorite } = req.body;
   try {
     const { error } = contactSchema.validate(req.body);
     if (error) {
@@ -58,28 +55,20 @@ const create = async (req, res, next) => {
       });
     }
 
-    const result = await service.createContact({
-      name,
-      email,
-      phone,
-      favorite,
-    });
+    const result = await service.createContact(req.user._id, req.body);
+
     res.status(201).json({
       status: "success",
       code: 201,
       data: { contact: result.toJSON() },
     });
   } catch (error) {
-    console.error("Error adding contact: ", error);
     next(error);
   }
 };
 
 const update = async (req, res, next) => {
   const { id } = req.params;
-  const { name, email, phone, favorite } = req.body;
-
-  console.log("Received payload for update:", req.body);
 
   try {
     const { error } = contactSchema.validate(req.body);
@@ -91,12 +80,7 @@ const update = async (req, res, next) => {
       });
     }
 
-    const result = await service.updateContact(id, {
-      name,
-      email,
-      phone,
-      favorite,
-    });
+    const result = await service.updateContact(req.user._id, id, req.body);
     if (result) {
       res.json({
         status: "success",
@@ -112,7 +96,6 @@ const update = async (req, res, next) => {
       });
     }
   } catch (error) {
-    console.error("Error updating contact: ", error);
     next(error);
   }
 };
@@ -130,7 +113,7 @@ const updateStatus = async (req, res, next) => {
   }
 
   try {
-    const result = await service.updateContact(id, { favorite });
+    const result = await service.updateContact(req.user._id, id, { favorite });
     if (result) {
       res.json({
         status: "success",
@@ -153,7 +136,7 @@ const updateStatus = async (req, res, next) => {
 const remove = async (req, res, next) => {
   const { id } = req.params;
   try {
-    const result = await service.removeContact(id);
+    const result = await service.removeContact(req.user._id, id);
     if (result) {
       res.json({
         status: "success",
@@ -169,7 +152,6 @@ const remove = async (req, res, next) => {
       });
     }
   } catch (error) {
-    console.error("Error removing contact: ", error);
     next(error);
   }
 };
